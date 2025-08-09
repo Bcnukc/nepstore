53f82ad871e34d4b2b3284faa9e60915065b3715
Bishnu kc <bishnu.kc7864@gmail.com>
2025-08-09 13:33:42 +0545

    Fix: correct deployment mistake

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -2,16 +2,20 @@
 
 import { prisma } from "@/lib/prisma";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { Product } from "@/types";
 
-import { convertToPlainObject } from "../utils";
-
-export async function getLatestProducts() {
+export async function getLatestProducts(): Promise<Product[]> {
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
   });
 
-  return convertToPlainObject(data);
+  return data.map((p) => ({
+    ...p,
+    price: Number(p.price), // convert string to number
+    rating: Number(p.rating), // convert string to number
+    createdAt: p.createdAt.toISOString(), // convert Date to string
+  }));
 }
 
 export async function getProductBySlug(slug: string) {
